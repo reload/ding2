cb7362ebe96f17ccd707f8eb9d0195b7dda3f78d
Kasper Garnaes <kasper.garnaes@gmail.com>
2019-05-14 15:39:24 +0200

    Add support for specifying an API token using a data attribute
    We assume that the server will be responsible for acquiring a token
    for accessing the OpenPlatform on behalf of the user. This way we
    avoid exposing our client id and secret to the frontend.
    
    To pass the token to the frontend we let the server generate it as a
    date attribute.

--- modules/ding_user_frontend_js/js/app/src/index.js
+++ modules/ding_user_frontend_js/js/app/src/index.js
@@ -2,5 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import UserDashboard from './components/Dashboard/UserDashboard';
 
-ReactDOM.render(<UserDashboard />, document.getElementById('ding-user-frontend-js-user-dashboard'));
-
+const root = document.getElementById('ding-user-frontend-js-user-dashboard');
+ReactDOM.render(<UserDashboard token={root.dataset.apiToken} />, root);
