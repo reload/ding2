8dca37b311c603cfcc7432cad33d099896ab1204
Kasper Garnaes <kasper.garnaes@gmail.com>
2019-05-14 15:36:51 +0200

    Initial attempt at asynchronous data loading
    We mimick the supposed data structure from the OpenPlatform using
    Mocky including a delay to show our loading state.

--- modules/ding_user_frontend_js/js/app/src/components/Dashboard/Title.js
+++ modules/ding_user_frontend_js/js/app/src/components/Dashboard/Title.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import classNames from 'classnames';
 
 class Title extends React.Component {
     render() {
+        const countClass = classNames({
+            'user-dashboard-title__count': true,
+            'user-dashboard-title__count--is-loading': this.props.loading
+        });
+
         return (
             <header className="user-dashboard-title">
                 {this.props.text}
-                <span className="user-dashboard-title__count">{this.props.count}</span>
+                <span className={countClass}>{this.props.count}</span>
             </header>
         )
     };
