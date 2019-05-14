9180e6328700b77994c8bb481a6e335638e85bf8
Kasper Garnaes <kasper.garnaes@gmail.com>
2019-05-14 15:36:51 +0200

    Initial attempt at asynchronous data loading
    We mimick the supposed data structure from the OpenPlatform using
    Mocky including a delay to show our loading state.

--- modules/ding_user_frontend_js/js/app/src/components/Dashboard/Item.js
+++ modules/ding_user_frontend_js/js/app/src/components/Dashboard/Item.js
@@ -1,13 +1,23 @@
 import React from 'react';
+import classNames from 'classnames';
 
 class Item extends React.Component {
     render() {
+        const sectionClass = classNames({
+            'user-dashboard-item': true,
+            ['user-dashboard-item--type-' + this.props.type]: this.props.type,
+            'user-dashboard-item--has-notification': this.props.count > 0
+        });
+        const countClass = classNames({
+            'user-dashboard-item__count': true,
+            'user-dashboard-item__count--is-loading': this.props.loading
+        });
         return (
-            <section className="user-dashboard-item user-dashboard-item--has-notification">
-                <span className="user-dashboard-item__count">{this.props.count}</span>
+            <section className={sectionClass} >
+                <span className={countClass}>{this.props.count}</span>
                 <div className="user-dashboard-item__text">
-                <header className="user-dashboard-item__title">{this.props.title}</header>
-                <p className="user-dashboard-item__description">{this.props.description}</p>
+                    <header className="user-dashboard-item__title">{this.props.title}</header>
+                    <p className="user-dashboard-item__description">{this.props.description}</p>
                 </div>
             </section>
         )
--- modules/ding_user_frontend_js/js/app/src/components/Dashboard/UserDashboard.js
+++ modules/ding_user_frontend_js/js/app/src/components/Dashboard/UserDashboard.js
@@ -1,21 +1,50 @@
 import React, { useState, useEffect } from 'react';
 import '../../css/UserDashboard.css';
+import { isPast, isFuture, differenceInDays } from 'date-fns';
 import Title from './Title';
 import Item from './Item';
 
-function UserDashboard() {
+const API_URL = 'http://www.mocky.io/v2/5cda8e37300000680068c7cc?mocky-delay=5000ms';
+
+function UserDashboard(props) {
+  const [data, setData] = useState({loans: [], orders: [], isFetching: false});
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setData({loans: data.loans, orders: data.orders, isFetching: true});
+        const response = await fetch(API_URL).then(response => response.json());
+        setData({loans: response['data.json'].loans, orders: response['data.json'].orders, isFetching: false});
+      } catch (e) {
+        console.log(e);
+        setData({loans: data.loans, orders: data.orders, isFetching: false});
+      }
+    };
+    fetchData();
+  }, []);
+
+  const numLoans = data.loans.length;
+  const numLoansOverdue = (data.loans.filter((loan => isPast(new Date(loan.dueDate))))).length;
+  // Loans are due soon when within 1 week of their due date.
+  const numLoansDueSoon = (data.loans.filter(loan => isFuture(new Date(loan.dueDate)) && differenceInDays(new Date(loan.dueDate), new Date()) > 7)).length;
+  const numOtherLoans = numLoans - numLoansOverdue - numLoansDueSoon;
+
+  const numOrders = data.orders.length;
+  const numOrdersPickup = (data.orders.filter(order => order.status === 'ready')).length;
+  const numOtherOrders = numOrders - numOrdersPickup;
+
   return (
     <div className="user-dashboard">
       <section className="user-dashboard__column">
-        <Title text={"Lån"} count={12}/>
-        <Item title={"Overskredet"} description={"Lån har overskredet afleveringsdatoen"} count={1}/>
-        <Item title={"Udløber snart"} description={"Lån udløber snart"} count={2} />
-        <Item title={"Dine lån"} description={"Lån med længere udløbstid"} count={9} />
+        <Title text={"Lån"} loading={data.isFetching} count={numLoans}/>
+        <Item type="loans-overdue" title={"Overskredet"} description={"Lån har overskredet afleveringsdatoen"} loading={data.isFetching} count={numLoansOverdue}/>
+        <Item type="loans-due" title={"Udløber snart"} description={"Lån udløber snart"} loading={data.isFetching} count={numLoansDueSoon} />
+        <Item type="loans-other" title={"Dine lån"} description={"Lån med længere udløbstid"} loading={data.isFetching} count={numOtherLoans} />
       </section>
       <section className="user-dashboard__column">
-        <Title text={"Reserveringer"} count={7}/>
-        <Item title={"Klar til afhentning"} description={"Reserveringer kan nu afhentes på biblioteket"} count={1} />
-        <Item title={"Du står i kø"} description={"Reserveringer du står i kø til"} count={0} />
+        <Title text={"Reserveringer"} loading={data.isFetching} count={numOrders}/>
+        <Item type="orders-ready" title={"Klar til afhentning"} description={"Reserveringer kan nu afhentes på biblioteket"} loading={data.isFetching} count={numOrdersPickup} />
+        <Item type="orders-other" title={"Du står i kø"} description={"Reserveringer du står i kø til"} loading={data.isFetching} count={numOtherOrders} />
       </section>
     </div>
   );
