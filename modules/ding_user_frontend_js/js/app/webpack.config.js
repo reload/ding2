d889536ebc3f213c3a21d46a784a78a74d7fad81
Lasse Borly <lasseborly@gmail.com>
2019-05-22 10:46:08 +0200

    Introduce custom webpack

---
+++ modules/ding_user_frontend_js/js/app/webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path')
+
+module.exports = (_env, argv) => {
+  const production = argv.mode !== 'development'
+  return {
+    entry: {
+      dashboard: './components/dashboard/bootstrap.js',
+    },
+    output: {
+      filename: '[name].js',
+      path: path.resolve(__dirname, 'dist')
+    },
+    devtool: production ? 'none' : 'inline-source-map',
+    devServer: {
+      contentBase: './'
+    },
+    optimization: {
+      runtimeChunk: 'single',
+      splitChunks: {
+        name: () => 'bundle',
+        chunks: 'all'
+      }
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader"
+          }
+        },
+        {
+          test: /\.css$/,
+          exclude: /node_modules/,
+          use: ['style-loader', 'css-loader']
+        }
+      ],
+    }
+  }
+}
