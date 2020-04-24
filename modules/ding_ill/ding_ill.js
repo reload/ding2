ab2eee8442e26ca9a88e2dceac2d9ba98eb2354a
Kasper Garnaes <kasper.garnaes@gmail.com>
2020-04-24 08:35:51 +0200

    Rework presentation of collapsed objects
    Instead of showing compact version of materials within a title group
    we hide them all by default and instead add a button to the first
    material of the group. Clicking the button will toggle the other 
    materials within the group. 
    
    This requires a rework of logic, behavior and styling as we need the
    groups to be available at all levels.

--- modules/ding_ill/ding_ill.js
+++ modules/ding_ill/ding_ill.js
@@ -6,12 +6,24 @@
 
   Drupal.behaviors.ding_ill_collapsible = {
     attach: function(context, settings) {
-      $('.js-ding-ill-collapsible', context).click(function(e) {
-        $(this).toggleClass('ding-ill-collapsed');
-        // Disable clicks/links if the material is collapsed. We only
-        // want to expand here.
-        if ($(this).hasClass('ding-ill-collapsed')) {
-          e.preventDefault();
+      $('.js-ding-ill-material-group', context).each(function() {
+        var group = $(this);
+        var materials = group.find('.js-ding-ill-material');
+        // Add a toggle element if the group has multiple materials.
+        if (materials.length > 1) {
+          var toggle = $(
+            '<div class="js-ding-ill-material-group-toggle ding-ill--material-group-toggle">' +
+              '<span class="ding-ill--material-group-toggle-text">' + Drupal.t('Show all editions') + '</span>' +
+            '</div>'
+          );
+          toggle.click(function() {
+            group.toggleClass('ding-ill--material-group__expanded');
+          });
+
+          // We need to append to the .inner element of the material because
+          // of all the floating.
+          materials.first().children()
+            .append(toggle);
         }
       });
     }
