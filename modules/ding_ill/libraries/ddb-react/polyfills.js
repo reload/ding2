89a4a816b5865396a3d145633f25d46426355344
Thomas Fini Hansen <xen@xen.dk>
2020-10-16 08:08:21 +0200

    Update ddb_react to latest version
    This pulls in 7688aa363173ac8d7905868e58fb2362344418e8

--- modules/ding_ill/libraries/ddb-react/polyfills.js
+++ modules/ding_ill/libraries/ddb-react/polyfills.js
@@ -1,2 +1,2 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{228:function(n,w,o){"use strict";o.r(w);o(229),o(236),o(240),o(243)}},[[228,1,0]]]);
+(window.webpackJsonp=window.webpackJsonp||[]).push([[9],{245:function(n,w,o){"use strict";o.r(w);o(246),o(253),o(256),o(259)}},[[245,1,0]]]);
 //# sourceMappingURL=polyfills.js.map
